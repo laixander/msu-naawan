aea2d59bcf6382b53c4b831c6fc2dd946223e71c
Laixander Naguit <laixander@gmail.com>
2024-01-01 22:19:52 +0800

    updates

--- pages/students.vue
+++ pages/students.vue
@@ -19,23 +19,28 @@
     
             <a-card title="List of Students" class="container-cards" :bordered="false">
                 <template #extra>
-                    <a-radio-group v-model:value="display">
+                    <a-button type="text">
+                        <template #icon>
+                            <BlockOutlined />
+                        </template>
+                    </a-button>
+                    <a-button type="text">
+                        <template #icon>
+                            <TableOutlined />
+                        </template>
+                    </a-button>
+                    <!-- <a-radio-group v-model:value="display">
                         <a-radio-button value="card"><CreditCardOutlined :style="iconSize" /> Card</a-radio-button>
                         <a-radio-button value="table"><TableOutlined :style="iconSize" /> Table</a-radio-button>
-                    </a-radio-group>
+                    </a-radio-group> -->
                 </template>
-                <a-flex gap="8">
-                    <a-card title="Hello World">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, qui.</p>
-                    </a-card>
-                    <a-card title="Hello World">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, qui.</p>
-                    </a-card>
-                    <a-card title="Hello World">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, qui.</p>
-                    </a-card>
-                    <a-card title="Hello World">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, qui.</p>
+                <a-flex gap="8" wrap="wrap">
+                    <a-card hoverable class="student-info" v-for="student in students" :key="student.id" >
+                        <a-avatar :size="72">
+                            <template #icon><UserOutlined /></template>
+                        </a-avatar>
+                        <a-typography-paragraph strong>{{ student.name }}</a-typography-paragraph>
+                        <a-typography-paragraph type="secondary">{{ student.number }}</a-typography-paragraph>
                     </a-card>
                 </a-flex>
             </a-card>
@@ -46,11 +51,9 @@
 <script lang="ts" setup>
     import { ref } from 'vue';
     import type { CSSProperties } from 'vue';
-    import { PlusOutlined } from '@ant-design/icons-vue';
+    import { PlusOutlined, BlockOutlined, TableOutlined, UserOutlined } from '@ant-design/icons-vue';
 
     const value = ref<string>('');
-
-    const display = ref<string>('card');
 
     const onSearch = (searchValue: string) => {
         console.log('use value', searchValue);
@@ -61,8 +64,43 @@
         width: '100%'
     };
 
-    const iconSize: CSSProperties = {
-        fontSize: '12px',
-        marginRight: '6px'
-    };
+    const students = reactive(
+        [
+            {
+                id: '1',
+                name: 'Paityn Botosh',
+                number: '2024-063010-51'
+            },
+            {
+                id: '2',
+                name: 'Tatiana Aminoff',
+                number: '2024-063010-51'
+            },
+            {
+                id: '3',
+                name: 'Ann Baptista',
+                number: '2024-063010-51'
+            },
+            {
+                id: '4',
+                name: 'Miracle Stanton',
+                number: '2024-063010-51'
+            },
+            {
+                id: '5',
+                name: 'Marley Herwitz',
+                number: '2024-063010-51'
+            },
+            {
+                id: '6',
+                name: 'Emerson Rosser',
+                number: '2024-063010-51'
+            },
+            {
+                id: '7',
+                name: 'Corey Donin',
+                number: '2024-063010-51'
+            },
+        ]
+    )
 </script>
